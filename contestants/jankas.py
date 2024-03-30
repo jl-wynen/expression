58981f66bd187bc4ce4f9f1822d0838d3ec81cbc
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-03-30 14:59:14 +0100

    Consider shorter plays

--- contestants/jankas.py
+++ contestants/jankas.py
@@ -2,6 +2,8 @@ import functools
 import itertools
 import multiprocessing as mp
 
+import numpy as np
+
 from game import base_agent, helpers
 
 
@@ -9,7 +11,7 @@ class Agent(base_agent.BaseAgent):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.name = "Jankas"
-        self.pool = mp.Pool(processes=4)
+        # self.pool = mp.Pool(processes=4)
 
     def select_cards(self):
         self.deck = dict(
@@ -23,8 +25,7 @@ class Agent(base_agent.BaseAgent):
         )
 
     def play_turn(self, hand, energy, game_info, locked_term, current_term):
-        # TODO also try early termination
-        return highest_value_play(hand, energy, locked_term, current_term, self.pool)
+        return highest_value_play(hand, energy, locked_term, current_term)
 
 
 def card_permutations(hand):
@@ -32,7 +33,7 @@ def card_permutations(hand):
         yield [hand[index] for index in indexes]
 
 
-def highest_value_play(hand, energy, locked_term, current_term, pool):
+def highest_value_play(hand, energy, locked_term, current_term):
     candidate = []
     candidate_value = locked_term + current_term
     kernel = functools.partial(check_play, energy=energy, locked_term=locked_term, current_term=current_term)
@@ -41,6 +42,7 @@ def highest_value_play(hand, energy, locked_term, current_term, pool):
     results = map(kernel, card_permutations(hand))
 
     for to_play, score in results:
+        # TODO tie: keep shorted to_play, tie: use most expensive
         if score > candidate_value:
             candidate = to_play
             candidate_value = score
@@ -49,10 +51,15 @@ def highest_value_play(hand, energy, locked_term, current_term, pool):
 
 def check_play(cards, energy, locked_term, current_term):
     to_play = []
+    intermediate_values = []
     for card in cards:
         if card.cost > energy:
             break
         energy -= card.cost
-        to_play.append(card)
         locked_term, current_term = helpers.apply_card(card, locked_term, current_term)
-    return to_play, locked_term + current_term
+        to_play.append(card)
+        intermediate_values.append(locked_term + current_term)
+    if not intermediate_values:
+        return [], locked_term + current_term
+    best_index = np.argmax(intermediate_values)
+    return to_play[:best_index + 1], intermediate_values[best_index]
