a3ddd67ebb77a03c0b8c21b480c93644af774884
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-03-30 15:14:52 +0100

    Use cheaper plays

--- contestants/jankas.py
+++ contestants/jankas.py
@@ -36,22 +36,25 @@ def card_permutations(hand):
 def highest_value_play(hand, energy, locked_term, current_term):
     candidate = []
     candidate_value = locked_term + current_term
+    candidate_cost = 0
     kernel = functools.partial(check_play, energy=energy, locked_term=locked_term, current_term=current_term)
     # results = pool.map(kernel, card_permutations(hand), chunksize=20)
     # results = pool.imap(kernel, card_permutations(hand), chunksize=20)
     results = map(kernel, card_permutations(hand))
 
-    for to_play, score in results:
-        # TODO tie: keep shorted to_play, tie: use most expensive
-        if score > candidate_value:
+    for to_play, score, cost in results:
+        if (score > candidate_value) or (
+                score == candidate_value and (len(to_play) < len(candidate) or cost < candidate_cost)):
             candidate = to_play
             candidate_value = score
+            candidate_cost = cost
     return candidate
 
 
 def check_play(cards, energy, locked_term, current_term):
     to_play = []
     intermediate_values = []
+    total_cost = 0
     for card in cards:
         if card.cost > energy:
             break
@@ -59,7 +62,8 @@ def check_play(cards, energy, locked_term, current_term):
         locked_term, current_term = helpers.apply_card(card, locked_term, current_term)
         to_play.append(card)
         intermediate_values.append(locked_term + current_term)
+        total_cost += card.cost
     if not intermediate_values:
-        return [], locked_term + current_term
+        return [], locked_term + current_term, 0
     best_index = np.argmax(intermediate_values)
-    return to_play[:best_index + 1], intermediate_values[best_index]
+    return to_play[:best_index + 1], intermediate_values[best_index], total_cost
